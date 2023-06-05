0d736bfae08a0c22a65bd3037fdabe8fc5533b54
Vladimir <skittez@mail.ru>
2023-06-05 12:01:11 +0300

    Initial commit

---
+++ 1/main.js
@@ -0,0 +1,41 @@
+'use strict';
+//var leftBorderWidth = 1;
+//{
+//    let second = 2;
+//}
+//const pi = 3.14;
+//
+//console.log (second);
+
+//let persone = {
+//    name: "John",
+//    age: 24,
+//    isMarried: false
+//}
+//
+//console.log (persone.name);
+//console.log (persone["name"]);
+//
+//let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+//
+//console.log (arr[0]);
+
+//let answer = confirm("are you here?");
+//console.log(answer);
+
+//let answer = prompt("are you 18 yo?", "Yes");
+//console.log(typeof(arr));
+
+let incr = 10,
+    decr = 10;
+
+//incr++;
+//decr--;
+
+console.log (incr++);
+console.log (decr--);
+
+
+let isChecked = true,
+    isClose   = true;
+
